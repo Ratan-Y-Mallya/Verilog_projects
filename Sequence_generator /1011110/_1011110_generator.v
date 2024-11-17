9b14f1365599a85efd7933556b5d641e0f926899
Ratan-Y-Mallya <161617838+Ratan-Y-Mallya@users.noreply.github.com>
2024-11-17 18:03:40 +0530

    Update _1011110_generator.v

--- Sequence_generator /1011110/_1011110_generator.v
+++ Sequence_generator /1011110/_1011110_generator.v
@@ -1,1 +1,17 @@
+module _1011110_generator( input  [3:0] in, input clk ,reset,output reg out   );
 
+ reg [3:0] shift_reg;
+
+always @(posedge clk or reset)
+begin
+if(reset) begin
+ shift_reg <=in;
+ out=0;
+ end
+ 
+else  begin
+shift_reg <= {~(shift_reg[0]&shift_reg[1]&shift_reg[3]),shift_reg[3:1]};
+out <= shift_reg[3];
+end
+end
+endmodule
