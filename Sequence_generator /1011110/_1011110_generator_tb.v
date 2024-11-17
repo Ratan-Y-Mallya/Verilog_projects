ab1edd3dd10ae40c2266f2e3b45b6e5d011c1361
Ratan-Y-Mallya <161617838+Ratan-Y-Mallya@users.noreply.github.com>
2024-11-17 18:05:08 +0530

    Update _1011110_generator_tb.v

--- Sequence_generator /1011110/_1011110_generator_tb.v
+++ Sequence_generator /1011110/_1011110_generator_tb.v
@@ -1,1 +1,27 @@
+`timescale 1ns / 1ps
+
+module _1011110_tb(  );
+
+reg [3:0] in;
+reg clk,reset;
+wire out ;
+
+_1011110_generator dut (in,clk,reset,out);
+
+initial begin 
+clk =1'b0;
+reset =1;
+
+forever #5 clk =~clk;
+end
+
+initial begin 
+reset =1;in = 4'b1011;
+#10 reset =1'b0;
+
+#100;
+$finish;
+end
+
+endmodule
 
